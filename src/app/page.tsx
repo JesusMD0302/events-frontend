e088a75fb401602a6d6f08da9711f39fc248d713
Jesus Mena <jesusalbertomena2002@gmail.com>
2024-07-15 21:02:53 -0600

    Change app routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-export default function Home() {
-  return (
-    <main>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, porro
-        dicta perspiciatis error enim animi molestiae aperiam, molestias modi
-        neque provident nemo asperiores sint, eaque necessitatibus voluptatem
-        dolorem quibusdam rerum facere. Quisquam?
-      </p>
-    </main>
-  );
+import { redirect } from "next/navigation";
+
+export default function Main() {
+  redirect("/app");
 }

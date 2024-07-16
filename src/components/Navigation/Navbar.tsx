ebc332b247c20b929282d6186bfd5c112ff7c78c
Jesus Mena <jesusalbertomena2002@gmail.com>
2024-07-15 21:38:29 -0600

    Change navbar component

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,3 +1,13 @@
+import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography component="p" variant="h6">
+          Events Management App
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }

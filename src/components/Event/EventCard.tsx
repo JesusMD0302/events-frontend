85793304d96614200345d2545200861a11d30aa7
Jesus Mena <jesusalbertomena2002@gmail.com>
2024-07-17 16:49:35 -0600

    Chanage EventCard component and add "Ver más" button

--- src/components/Event/EventCard.tsx
+++ src/components/Event/EventCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import {
   Box,
+  Button,
   Card,
   CardActionArea,
   CardContent,
@@ -9,7 +10,7 @@ import {
 } from "@mui/material";
 import EventMap from "./EventMap";
 import { EventApp } from "@/types/event";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 type EventOmited = Omit<EventApp, "author" | "guests" | "cost" | "attender">;
 
@@ -18,65 +19,61 @@ interface EventCardProps {
 }
 
 export default function EventCard() {
-  const router = useRouter();
-  const handleClick = () => {
-    router.push("/app/events/1");
-  };
-
   return (
     <Card
+      // elevation={3}
+      variant="outlined"
       sx={{
-        cursor: "pointer",
+        display: "flex",
+        flexDirection: { xs: "column", md: "row" },
+        alignItems: "stretch",
       }}
     >
-      <CardActionArea
-        onClick={handleClick}
-        sx={{
-          display: "flex",
-          flexDirection: { xs: "column", md: "row" },
-        }}
-      >
-        <CardContent>
-          <Box component="header" width={{ md: "150px" }}>
-            <Typography variant="h5" component="h2">
-              Evento
-            </Typography>
-            <Typography component="p" color="textSecondary">
-              <Typography component="span" color="textSecondary">
-                01/01/2022
-              </Typography>
-              {" - "}
-              <Typography component="span" color="textSecondary">
-                10:00
-              </Typography>
-            </Typography>
-          </Box>
-          <Typography
-            variant="body2"
-            component="p"
-            mt={2}
-            sx={{
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              wordBreak: "break-word",
-              display: "-webkit-box",
-              WebkitBoxOrient: "vertical",
-              WebkitLineClamp: 5,
-            }}
-          >
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Exercitationem et rem nesciunt consequatur quos ipsam enim expedita,
-            temporibus numquam inventore vitae tempora beatae cum fuga ullam
-            veniam voluptatum? Totam quisquam ea autem architecto ex atque
-            voluptatum, modi quo adipisci vel cum voluptas laborum sed explicabo
-            natus eos perspiciatis quia aut vitae quis dolores veniam mollitia
-            libero?
+      <CardContent>
+        <Box component="header" width={{ md: "150px" }}>
+          <Typography variant="h5" component="h2">
+            Evento
           </Typography>
-        </CardContent>
-        <CardMedia sx={{ width: "100%" }}>
-          <EventMap />
-        </CardMedia>
-      </CardActionArea>
+          <Typography component="p" color="textSecondary">
+            <Typography component="span" color="textSecondary">
+              01/01/2022
+            </Typography>
+            {" - "}
+            <Typography component="span" color="textSecondary">
+              10:00
+            </Typography>
+          </Typography>
+        </Box>
+        <Typography
+          variant="body2"
+          component="p"
+          mt={2}
+          sx={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            wordBreak: "break-word",
+            display: "-webkit-box",
+            WebkitBoxOrient: "vertical",
+            WebkitLineClamp: 5,
+          }}
+        >
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Exercitationem et rem nesciunt consequatur quos ipsam enim expedita,
+          temporibus numquam inventore vitae tempora beatae cum fuga ullam
+          veniam voluptatum? Totam quisquam ea autem architecto ex atque
+          voluptatum, modi quo adipisci vel cum voluptas laborum sed explicabo
+          natus eos perspiciatis quia aut vitae quis dolores veniam mollitia
+          libero?
+        </Typography>
+        <Link href="/app/events/1" passHref>
+          <Button variant="text" size="small" sx={{ mt: 2 }}>
+            Ver más
+          </Button>
+        </Link>
+      </CardContent>
+      <CardMedia sx={{ width: "100%" }}>
+        <EventMap />
+      </CardMedia>
     </Card>
   );
 }

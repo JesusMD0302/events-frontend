0f1309252466aae35775e622ed417504dc1f5cae
Jesus Mena <jesusalbertomena2002@gmail.com>
2024-07-17 15:21:15 -0600

    Change Events Calendar component and add full calendar component

--- src/app/app/events/calendar/page.tsx
+++ src/app/app/events/calendar/page.tsx
@@ -1,3 +1,10 @@
-export default function EventsCalendar() {
-  return <div>page</div>;
+import EventsCalendar from "@/components/Event/EventsCalendar";
+import { Box } from "@mui/material";
+
+export default function Calendar() {
+  return (
+    <Box component="section" height="100%" maxHeight="870px">
+      <EventsCalendar />
+    </Box>
+  );
 }
--- src/components/Event/EventsCalendar.tsx
+++ src/components/Event/EventsCalendar.tsx
@@ -1,3 +1,52 @@
+"use client";
+import { Box } from "@mui/material";
+import { useRouter } from "next/navigation";
+// Fullcalendar
+import FullCalendar from "@fullcalendar/react";
+// Fullcalendar plugins
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
 export default function EventsCalendar() {
-  return <div>EventsCalendar</div>;
+  const router = useRouter();
+
+  const handleSelectEvent = (event: { id: number }) => {
+    router.push(`/app/events/${event.id}`);
+  };
+
+  return (
+    <Box component="section" height="100%">
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        dayMaxEventRows={true}
+        height={"auto"}
+        aspectRatio={1.3}
+        dateClick={(arg) => {
+          alert(arg.dateStr);
+        }}
+        eventClick={(arg) => {
+          handleSelectEvent(arg.event as any);
+        }}
+        views={{
+          dayGridMonth: {
+            dayMaxEventRows: 3,
+          },
+        }}
+        events={[
+          { id: "1", title: "event 1", date: "2024-07-01" },
+          { id: "2", title: "event 2", date: "2024-07-02" },
+          { id: "3", title: "event 3", date: "2024-07-02" },
+          { id: "4", title: "event 4", date: "2024-07-02" },
+          { id: "5", title: "event 5", date: "2024-07-02" },
+          { id: "6", title: "event 6", date: "2024-07-02" },
+          { id: "7", title: "event 7", date: "2024-07-02" },
+          { id: "8", title: "event 8", date: "2024-07-02" },
+          { id: "8", title: "event 8", date: "2024-07-02" },
+          { id: "8", title: "event 8", date: "2024-07-02" },
+          { id: "8", title: "event 8", date: "2024-07-02" },
+        ]}
+      />
+    </Box>
+  );
 }

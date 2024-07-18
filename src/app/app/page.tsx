290db09aec1a605fd94a569a75ea23ce6cef506b
Jesus Mena <jesusalbertomena2002@gmail.com>
2024-07-18 11:21:37 -0600

    Change Home component to fetch events with authorization token

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,13 +1,38 @@
 import EventsContainer from "@/components/Event/EventsContainer";
+import api from "@/lib/api";
+import { authOptions } from "@/lib/auth";
+import { EventApp } from "@/types/event";
 import { Box, Typography } from "@mui/material";
+import { getServerSession, Session } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  const { user } = session as Session;
+  const token = user.token;
+
+  if (!token) {
+    return { redirect: { destination: "/auth/signin", permanent: false } };
+  }
+
+  let events: EventApp[] = [];
+  try {
+    const res = await api.get("/events/getEvent", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    events = res.data;
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
     <Box component="section">
       <Typography component="h1" variant="h4">
         Eventos cercanos
       </Typography>
-      <EventsContainer />
+      <EventsContainer events={events} />
     </Box>
   );
 }
--- src/components/Event/EventsContainer.tsx
+++ src/components/Event/EventsContainer.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import EventCard from "./EventCard";
 import { EventApp } from "@/types/event";
 
@@ -6,7 +6,7 @@ interface EventsContainerProps {
   events: EventApp[];
 }
 
-export default function EventsContainer() {
+export default function EventsContainer({ events }: EventsContainerProps) {
   return (
     <Box
       component="section"
@@ -15,9 +15,24 @@ export default function EventsContainer() {
       gridTemplateColumns={{ sm: "1fr", md: "1fr 1fr", lg: "repeat(3, 1fr)" }}
       gap={2}
     >
-      {[1, 2, 3, 4, 5].map((event) => (
-        <EventCard key={event} />
-      ))}
+      {events.length === 0 && (
+        <Typography
+          component="p"
+          gridColumn="1 / -1"
+          height={300}
+          variant="body1"
+          align="center"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          width="100%"
+        >
+          No hay eventos disponibles
+        </Typography>
+      )}
+
+      {events.length > 0 &&
+        events.map((event, index) => <EventCard key={index} />)}
     </Box>
   );
 }

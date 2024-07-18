04d5ed080ab150217e01f67096a8fadcb71978b3
Jesus Mena <jesusalbertomena2002@gmail.com>
2024-07-18 01:01:50 -0600

    Change sign-up form to add validation and submission logic

--- src/components/Auth/SignUpForm.tsx
+++ src/components/Auth/SignUpForm.tsx
@@ -1,21 +1,76 @@
 "use client";
 
+import useAxios from "@/hooks/useAxios";
 import {
   Box,
   Button,
   Card,
   CardContent,
-  CardHeader,
+  CircularProgress,
   Stack,
   TextField,
   Typography,
 } from "@mui/material";
+import { AxiosError } from "axios";
+import { useFormik } from "formik";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import * as Yup from "yup";
+
+const signUpValidationSchema = Yup.object({
+  username: Yup.string().required("El nombre de usuario es requerido"),
+  email: Yup.string()
+    .email("El correo electrónico no es válido")
+    .required("El correo electrónico es requerido"),
+  password: Yup.string()
+    .min(8, "La contraseña debe tener al menos 8 caracteres")
+    .required("La contraseña es requerida"),
+});
 
 export default function SignUpForm() {
+  const api = useAxios();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: signUpValidationSchema,
+    onSubmit: async (values, helpers) => {
+      try {
+        const res = await api.post("/user/signUp", values);
+        helpers.setStatus(null);
+
+        const result = await signIn("credentials", {
+          redirect: false,
+          email: values.email,
+          password: values.password,
+        });
+
+        if (result?.error) {
+          router.push("/auth/signin");
+        } else {
+          router.push("/app/");
+        }
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          if (error.response?.status === 400) {
+            helpers.setStatus(error.response.data.message);
+          }
+        }
+      }
+    },
+  });
+
   return (
-    <Box component="form">
+    <Box component="form" onSubmit={formik.handleSubmit}>
       <Card>
         <CardContent sx={{ padding: 0, ":last-child": { paddingBottom: 0 } }}>
           <Stack
@@ -25,7 +80,7 @@ export default function SignUpForm() {
             width="100%"
             maxWidth="1000px"
             height="90vh"
-            maxHeight="500px"
+            maxHeight="550px"
           >
             <Box
               component="section"
@@ -79,27 +134,74 @@ export default function SignUpForm() {
               <Box component="section" mt={4}>
                 <Stack spacing={2}>
                   <TextField
+                    label="Nombre de usuario"
+                    id="username"
+                    name="username"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={
+                      formik.touched.username && Boolean(formik.errors.username)
+                    }
+                    helperText={
+                      formik.touched.username && formik.errors.username
+                    }
+                    value={formik.values.username}
+                    fullWidth
+                    margin="normal"
+                    autoComplete="off"
+                  />
+                  <TextField
                     label="Correo electrónico"
+                    id="email"
+                    name="email"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                    value={formik.values.email}
                     fullWidth
                     margin="normal"
                     autoComplete="off"
                   />
                   <TextField
                     label="Contraseña"
+                    id="password"
+                    name="password"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={
+                      formik.touched.password && Boolean(formik.errors.password)
+                    }
+                    helperText={
+                      formik.touched.password && formik.errors.password
+                    }
+                    value={formik.values.password}
                     fullWidth
                     margin="normal"
                     type="password"
                   />
                 </Stack>
+
                 <Button
                   type="submit"
                   color="primary"
                   fullWidth
                   variant="contained"
                   sx={{ marginTop: 2 }}
+                  disabled={formik.isSubmitting}
+                  startIcon={
+                    formik.isSubmitting && (
+                      <CircularProgress size={16} color="inherit" />
+                    )
+                  }
                 >
                   Crear cuenta
                 </Button>
+                {formik.status && (
+                  <Typography color="error" align="left">
+                    {formik.status}
+                  </Typography>
+                )}
                 <Link href="/auth/signin" passHref>
                   <Button variant="text" fullWidth sx={{ marginTop: 2 }}>
                     ¿Ya tienes una cuenta? Inicia sesión

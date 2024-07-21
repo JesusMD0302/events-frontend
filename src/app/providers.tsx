f9a2b32949fbf0805abc4bd59c8b38f9c6316988
Jesus Mena <jesusalbertomena2002@gmail.com>
2024-07-20 21:01:35 -0600

    Add APIProvider to Providers component for Google Maps integration

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import { APIProvider } from "@vis.gl/react-google-maps";
 import { SessionProvider } from "next-auth/react";
+
+const apiKey = process.env.NEXT_PUBLIC_MAPS_API_KEY ?? "";
 
 export default function Providers({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <APIProvider apiKey={apiKey}>{children}</APIProvider>
+    </SessionProvider>
+  );
 }

4edeab0d3fb8d3fe190327d9a7bb1d865b7b0604
Jesus Mena <jesusalbertomena2002@gmail.com>
2024-07-20 21:13:45 -0600

    Change EventsCalendar page to fetch events and show them in the calendar

--- src/app/app/events/calendar/page.tsx
+++ src/app/app/events/calendar/page.tsx
@@ -1,7 +1,32 @@
 import EventsCalendar from "@/components/Event/EventsCalendar";
+import api from "@/lib/api";
+import { authOptions } from "@/lib/auth";
+import { EventApp } from "@/types/event";
 import { Box, Typography } from "@mui/material";
+import { getServerSession, Session } from "next-auth";
 
-export default function Calendar() {
+export default async function Calendar() {
+  const session = await getServerSession(authOptions);
+  const { user } = session as Session;
+  const token = user.token;
+
+  if (!token) {
+    return { redirect: { destination: "/auth/signin", permanent: false } };
+  }
+
+  let events: EventApp[] = [];
+  try {
+    const res = await api.get("/events/getEvent", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    events = res.data;
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
     <Box component="section" height="100%" maxHeight="870px">
       <Typography component="h1" variant="h4" fontWeight={700}>
@@ -12,7 +37,7 @@ export default function Calendar() {
         para ver más información y seleccionar un fecha para crear uno nuevo.
       </Typography>
 
-      <EventsCalendar />
+      <EventsCalendar events={events} />
     </Box>
   );
 }

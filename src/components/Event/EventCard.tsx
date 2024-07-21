4e0fd2551b5b5f85079fd63bc1aebb292126e07e
Jesus Mena <jesusalbertomena2002@gmail.com>
2024-07-20 21:08:50 -0600

    Change EventCard component to display event information

--- src/components/Event/EventCard.tsx
+++ src/components/Event/EventCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 import {
+  Avatar,
   Box,
   Button,
   Card,
@@ -12,35 +13,55 @@ import EventMap from "./EventMap";
 import { EventApp } from "@/types/event";
 import Link from "next/link";
 
-type EventOmited = Omit<EventApp, "author" | "guests" | "cost" | "attender">;
+type EventOmited = Omit<EventApp, "guests" | "cost" | "attender">;
 
 interface EventCardProps {
   event: EventOmited;
 }
 
-export default function EventCard() {
+export default function EventCard({ event }: EventCardProps) {
   return (
     <Card
       // elevation={3}
       variant="outlined"
       sx={{
-        display: "flex",
-        flexDirection: { xs: "column", md: "row" },
+        display: "grid",
+        gridTemplateColumns: { md: "1fr 2fr" },
         alignItems: "stretch",
       }}
     >
       <CardContent>
-        <Box component="header" width={{ md: "150px" }}>
-          <Typography variant="h5" component="h2">
-            Evento
+        <Box component="header" width={{ md: "170px" }}>
+          <Typography
+            variant="subtitle1"
+            component="h3"
+            display="flex"
+            fontWeight={400}
+            gap={1}
+            alignItems="center"
+          >
+            <Avatar
+              sx={{ width: 24, height: 24 }}
+              title={event.author.username}
+            />
+            {event.author.username}
+          </Typography>
+          <Typography variant="h5" component="h2" mt={2}>
+            {event.name}
           </Typography>
           <Typography component="p" color="textSecondary">
             <Typography component="span" color="textSecondary">
-              01/01/2022
+              {new Date(event.date_time).toLocaleDateString()}
             </Typography>
             {" - "}
             <Typography component="span" color="textSecondary">
-              10:00
+              {new Date(event.date_time)
+                .toLocaleTimeString([], {
+                  hour: "numeric",
+                  minute: "numeric",
+                  hour12: true,
+                })
+                .replace(". ", ".")}
             </Typography>
           </Typography>
         </Box>
@@ -57,22 +78,21 @@ export default function EventCard() {
             WebkitLineClamp: 5,
           }}
         >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Exercitationem et rem nesciunt consequatur quos ipsam enim expedita,
-          temporibus numquam inventore vitae tempora beatae cum fuga ullam
-          veniam voluptatum? Totam quisquam ea autem architecto ex atque
-          voluptatum, modi quo adipisci vel cum voluptas laborum sed explicabo
-          natus eos perspiciatis quia aut vitae quis dolores veniam mollitia
-          libero?
+          {event.description}
         </Typography>
-        <Link href="/app/events/1" passHref>
+        <Link href={`/app/events/${event._id}`} passHref>
           <Button variant="text" size="small" sx={{ mt: 2 }}>
             Ver más
           </Button>
         </Link>
       </CardContent>
       <CardMedia sx={{ width: "100%" }}>
-        <EventMap />
+        <EventMap
+          location={event.location}
+          customWidth="100%"
+          customZoom={17}
+          disableDoubleClickZoom
+        />
       </CardMedia>
     </Card>
   );

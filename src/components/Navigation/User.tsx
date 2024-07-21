cddd930b896f29ea0ce94665afc6aa5a46fca3a6
Jesus Mena <jesusalbertomena2002@gmail.com>
2024-07-20 23:36:11 -0600

    Add User navigation component

---
+++ src/components/Navigation/User.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { AccountCircle, Logout } from "@mui/icons-material";
+import { IconButton, ListItemIcon, Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
+import { signOut } from "next-auth/react";
+
+const menuId = "account-menu";
+
+export default function User() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleSignOut = () => {
+    setAnchorEl(null);
+    signOut();
+  };
+
+  return (
+    <>
+      <IconButton
+        size="large"
+        edge="end"
+        aria-controls={menuId}
+        aria-haspopup="true"
+        onClick={handleProfileMenuOpen}
+        color="inherit"
+      >
+        <AccountCircle />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        id={menuId}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={isMenuOpen}
+        onClose={handleMenuClose}
+      >
+        <MenuItem onClick={handleSignOut}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Cerrar sesión
+        </MenuItem>
+      </Menu>
+    </>
+  );
+}

3dc976cd8aa7ba25774a982e15be49a899997cdd
Jesus Mena <jesusalbertomena2002@gmail.com>
2024-07-20 21:00:22 -0600

    Add redirects to next.config.mjs for improved routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/app",
+        permanent: true,
+      },
+      {
+        source: "/app/events",
+        destination: "/app/events/calendar",
+        permanent: true,
+      },
+      {
+        source: "/app/create",
+        destination: "/app/create/event",
+        permanent: true,
+      },
+      {
+        source: "/app/user",
+        destination: "/app/user/events",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
